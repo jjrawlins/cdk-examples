22dbf3ead4a250f7b413b6ae063fbb152af18267
Jayson Rawlins <jayson.rawlins@layerborn.io>
2024-04-09 22:34:18 -0400

    deployed custom resource and it succeed

--- projen-typescript/cdk-custom-resource/.projenrc.ts
+++ projen-typescript/cdk-custom-resource/.projenrc.ts
@@ -1,16 +1,31 @@
 import { awscdk } from 'projen';
 
+const cdkVersion = '2.133.0';
+const minNodeVersion = '18.0.0';
+
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.133.0',
+  cdkVersion: cdkVersion,
   defaultReleaseBranch: 'main',
   name: 'cdk-custom-resource',
-  minNodeVersion: '20.0.0',
+  minNodeVersion: minNodeVersion,
   projenrcTs: true,
+  lambdaOptions: {
+    runtime: awscdk.LambdaRuntime.NODEJS_18_X,
+  },
   gitignore: [
     'cdk.context.json',
     '.idea',
     '.DS_Store',
     '.vscode',
   ],
+  deps: [
+    'aws-lambda',
+    '@types/node@^18',
+    '@types/aws-lambda',
+  ],
+  devDeps: [
+    'jest-cdk-snapshot',
+  ],
+
 });
 project.synth();
